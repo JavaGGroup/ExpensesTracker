a8487d722f6df6f68a61317a4e0157e2c3503f8a
AWoszczyna <agnieszka.woszczyna2@mail.dcu.ie>
2019-11-25 22:47:48 +0000

    Adding Category class

---
+++ src/main/java/Category.java
@@ -0,0 +1,21 @@
+
+public class Category {
+    private long id;
+    private String name;
+
+    public Category(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
+
